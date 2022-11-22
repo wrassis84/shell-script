b1199a5d8904c85a1a21a00e509badd1084724dc
William Rezende <wrassis84@gmail.com>
2022-11-22 17:06:52 -0300

    feat: adds double quoted variable values

---
+++ mod1/variaveis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash 
+
+# When using variable values with spaces and line brakes...
+NOME="William Ramos
+de Assis Rezende"
+
+# We must use double quoted to read or show the variable values.
+# This will preserve the variable formatting!
+echo "$NOME"
