a071300c6c30f0fb7d5f151bb60cb2294226832c
William Rezende <wrassis84@gmail.com>
2022-11-23 16:45:23 -0300

    docs: adds practical exercises and rename files

---
+++ mod1/task1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ $1 -gt 10 ]
+then
+  echo "Script name: $0"
+  echo "Script PID: $$"
+fi
---
+++ mod1/task1_clean.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+[ $1 -gt 10 ] && echo "Script name: $0 | Script PID: $$"
