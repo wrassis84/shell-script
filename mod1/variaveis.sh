110741f55444751053c712a35a6b08cc251f83af
William Rezende <wrassis84@gmail.com>
2022-11-23 14:11:14 -0300

    feat: add positional params and others

--- mod1/variaveis.sh
+++ mod1/variaveis.sh
@@ -22,3 +22,21 @@ echo "$TOTAL"
 CAT_OUTPUT="$(cat /etc/passwd | grep william)"
 
 echo "$CAT_OUTPUT"
+
+echo ""
+
+echo "---------------------------------------------"
+
+echo ""
+
+echo "Name of this program: $0"
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
+echo "Parameter 3: $3"
+echo "Total of parameteres: $#"
+echo "All parameteres: $*"
+echo "All parameteres: $@"
+echo "PID of this program: $$"
+echo "PID of latest job in background: $!"
+echo "Latest parameter of latest run command: $_"
+echo "Latest command return code: $?"
