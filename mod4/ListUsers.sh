918828f13dcecdc860a83d362fc243912229ca11
William Rezende <wrassis84@gmail.com>
2022-12-07 15:45:28 -0300

    feat: adds custom debug function that shows debug messages colored according to       chosen debug level.

--- mod4/ListUsers.sh
+++ mod4/ListUsers.sh
@@ -22,19 +22,21 @@
 #
 ###############################################################################
 ### CHANGELOG :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
+# v1.4 07/12/2022, William Ramos de Assis Rezende:
+#  - Added custom debug function that shows debug messages colored according
+#    to chosen debug level.
 # v1.3 26/11/2022, William Ramos de Assis Rezende:
-#      - Added WHILE statement plus SHIFT and input validation;
-#      - Added suport for using 2 options at sametime;
+#  - Added WHILE statement plus SHIFT and input validation;
+#  - Added suport for using 2 options at sametime;
 # v1.2 26/11/2022, William Ramos de Assis Rezende:
-#      - Added -s option for sort output;
-#      - Added -u option for uppercase output;
+#  - Added -s option for sort output;
+#  - Added -u option for uppercase output;
 # v1.1 26/11/2022, William Ramos de Assis Rezende:
-#      - Changed IF for CASE statement;
-#      - Added "basename" statement;
+#  - Changed IF for CASE statement;
+#  - Added "basename" statement;
 # v1.0 25/11/2022, William Ramos de Assis Rezende:
-#      - Program's first version;
-#      - Added -h and -v options;
+#  - Program's first version;
+#  - Added -h and -v options;
 #
 ###############################################################################
 ### TESTING ENVIRONMENT :::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -55,12 +57,27 @@ USAGE_MESSAGE="
    NOTE: When using two options we must place them like this:
          ./ListUsers.sh -s -u
 "
-VERSION="v1.3"
 SORT_OUT=0
 UPPERCASE=0
+# Colors for debug messages according level:
+GREEN="\033[32;1m"
+YELLOW="\033[33;1m"
+RED="\033[31;1m"
+ESC="\033[m"
+
+VERSION="v1.3"
 #
 ################################################################################
 ### FUNCTION DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#
+#   The "debug_func" function below displays debug messages according to desired
+#   debug level.
+#   To use it, just place it on the desired line with the debug level as an
+#   argument and the "DEBUG MESSAGE" between double quotes. We can use the
+#   following levels:
+#   1 - generic messages          (location, eg: START/END of the program);
+#   2 - flow location messages    (eg entering/leaving WHILE);
+#   3 - content of important vars (e.g. $VAR's value before/after incrementing)
 #
 debug_func() {
   [ $1 -le $DEBUG_LEVEL ] || return
@@ -69,7 +86,7 @@ debug_func() {
     1) pref="| " && suf=" |" && echo -e  "${GREEN}$pref$*$suf${ESC}";;
     2) pref="| " && suf=" |" && echo -e "${YELLOW}$pref$*$suf${ESC}";;
     3) pref="| " && suf=" |" && echo -e    "${RED}$pref$*$suf${ESC}";;
-    *)                          echo      "Uncategorized Message!: $*";return;;
+    *)                          echo       "Uncategorized Message!: $*";return;;
   esac
   #shift
 }
@@ -80,20 +97,25 @@ debug_func() {
 ###############################################################################
 ### BEGIN OF CODE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
-while test -n "$1"
-do
+debug_func 1 "Program start!"
+debug_func 2 "Entering WHILE!"
+while test -n "$1"; do
   case "$1" in
     -h) echo "$USAGE_MESSAGE" && exit 0     ;;
     -v) echo "$VERSION" && exit 0           ;;
+    debug_func 3 "\$SORT_OUT's value before choosing: $SORT_OUT"
     -s) SORT_OUT=1                          ;;
+    debug_func 3 "\$SORT_OUT's value after choosing: $SORT_OUT"
+    debug_func 3 "\$UPPERCASE's value before choosing: $UPPERCASE"
     -u) UPPERCASE=1                         ;;
+    debug_func 3 "\$UPPERCASE's value after choosing: $UPPERCASE"
      *) echo -en "Invalid Option!\n " && echo -en "$USAGE_MESSAGE" \
         && echo -en "\n[ENTER] to continue: " && read \
         && clear && exit 0;;
   esac
   shift
 done
-
+debug_func 2 "Leaving WHILE!"
 #set -xv # Uncomment for turn on code debugging :)
 
 [ $SORT_OUT -eq 1 ]  && USERS=$(echo "$USERS" | sort)
@@ -102,6 +124,7 @@ done
 #set +xv # Uncomment for turn off code debugging :(
 
 echo "$USERS"
+debug_func 1 "End of program!"
 #
 ### END OF CODE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ###############################################################################
