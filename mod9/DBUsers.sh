639af76ee0cd7aa800ff08556fbbb6b9a0fee99f
William Rezende <wrassis84@gmail.com>
2022-12-21 20:39:27 -0300

    feat: add function that parses the database and lists its users

--- mod9/DBUsers.sh
+++ mod9/DBUsers.sh
@@ -42,6 +42,24 @@ VERSION="v1.0"
 ################################################################################
 ### FUNCTION DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
+listusers_func () {
+  while read -r line
+  do
+    [ "$(echo $line | cut -c1)" = "#" ] && continue #skip commented lines
+    [ ! "$line" ] && continue #skip empty lines
+
+    local login="$(echo $line | cut -d: -f1)"
+    local name="$(echo $line | cut -d: -f2)"
+    local age="$(echo $line | cut -d: -f3)"
+    local gender="$(echo $line | cut -d: -f4)"
+
+    echo -e "${YELLOW}login: $login"
+    echo -e "${YELLOW}name: $name"
+    echo -e "${YELLOW}age: $age"
+    echo -e "${YELLOW}gender: $gender"
+  done < "$DB_FILE"
+}
+#
 ################################################################################
 ### TESTS/VALIDATIONS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Does the database file exist?
