d1212d5f6c8cea4b2c7b8687302d9c65af21b12f
William Rezende <wrassis84@gmail.com>
2022-12-22 07:37:30 -0300

    style: change TESTS/VALIDATIONS section

--- mod9/DBUsers.sh
+++ mod9/DBUsers.sh
@@ -26,44 +26,6 @@
 # zsh 5.8.1
 #
 ################################################################################
-### VARIABLE DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
-DB_FILE="Users.txt" # Database file
-
-RED="\033[31;1m"    #|
-GREEN="\033[32;1m"  #|
-YELLOW="\033[33;1m" #| Colors for output: 
-PURPLE="\033[35;1m" #|
-CIAN="\033[36;1m"   #|
-ESC="\033[m"
-
-VERSION="v1.0"
-#
-################################################################################
-### FUNCTION DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
-extractfields_func () {
-    local login="$(echo $1 | cut -d: -f1)"
-    local name="$(echo $1 | cut -d: -f2)"
-    local age="$(echo $1 | cut -d: -f3)"
-    local gender="$(echo $1 | cut -d: -f4)"
-
-    echo -e "${YELLOW}login:  $login"
-    echo -e "${YELLOW}name:   $name"
-    echo -e "${YELLOW}age:    $age"
-    echo -e "${YELLOW}gender: $gender"
-}
-
-listusers_func () {
-  while read -r line
-  do
-    [ "$(echo $line | cut -c1)" = "#" ] && continue #skip commented lines
-                          [ ! "$line" ] && continue #skip empty lines
-    extractfields_func "$line"
-  done < "$DB_FILE"
-}
-#
-################################################################################
 ### TESTS/VALIDATIONS ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Does the database file exist?
 [ ! -e "$DB_FILE" ] \
@@ -80,6 +42,45 @@ listusers_func () {
         && echo    "${RED}ERROR: No write permission on '$DB_FILE'!"${ESC} \
         && echo -n "[ENTER] to continue:" && read REPLY && clear           \
         && exit 1
+#
+################################################################################
+### VARIABLE DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#
+DB_FILE="Users.txt" # Database file
+SEP=:
+
+RED="\033[31;1m"    #|
+GREEN="\033[32;1m"  #|
+YELLOW="\033[33;1m" #| Colors for output: 
+PURPLE="\033[35;1m" #|
+CIAN="\033[36;1m"   #|
+ESC="\033[m"
+
+VERSION="v1.0"
+#
+################################################################################
+### FUNCTION DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#
+extractfields_func () {
+    local login="$(echo $1 | cut -d $SEP -f 1)"
+    local name="$(echo $1 | cut -d $SEP -f 2)"
+    local age="$(echo $1 | cut -d $SEP -f 3)"
+    local gender="$(echo $1 | cut -d $SEP -f 4)"
+
+    echo -e "${YELLOW}login:  $login"
+    echo -e "${YELLOW}name:   $name"
+    echo -e "${YELLOW}age:    $age"
+    echo -e "${YELLOW}gender: $gender"
+}
+
+listusers_func () {
+  while read -r line
+  do
+    [ "$(echo $line | cut -c1)" = "#" ] && continue #skip commented lines
+                          [ ! "$line" ] && continue #skip empty lines
+    extractfields_func "$line"
+  done < "$DB_FILE"
+}
 #
 ################################################################################
 ### BEGIN OF CODE ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
